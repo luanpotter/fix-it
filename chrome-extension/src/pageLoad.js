753b37b44795063a7e9bd308ec4199b01224021d
Luan Nico <luanpotter28@hotmail.com>
2015-02-28 17:18:27 -0300

    get url at event

--- chrome-extension/src/pageLoad.js
+++ chrome-extension/src/pageLoad.js
@@ -1,5 +1,7 @@
 (function () {
   $(document).ready(function () {
+    var url = document.URL;
+    console.log(url);
     chrome.runtime.sendMessage({ method: 'getFix', name: 'loghue' }, function(response) {
       eval(response.value);
     });
