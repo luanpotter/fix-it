7ce1e14525154fb636295a5e33ee0e8a50f8de9b
Luan Nico <luanpotter27@gmail.com>
2015-08-28 17:29:33 -0300

    drafts of new fix creation

--- chrome-extension/src/Server.js
+++ chrome-extension/src/Server.js
@@ -7,18 +7,12 @@ FixIt.Server = (function () {
     c.baseUrl(server);
   });
 
-  var list = function (url, callback) {
-    var domain = URI(url).hostname();
-    yawp('/fix').where('domain', '=', domain).list(callback);
+  var api = yawp('/fix');
+
+  api.find = function (domain, callback) {
+    api.where('domain', '=', domain).list(callback);
   };
 
-  var create = function (obj) {
-    yawp.save(obj);
-  };
-
-  return {
-    list : list,
-    create : create
-  };
+  return api;
 
 }());
--- chrome-extension/src/popup.js
+++ chrome-extension/src/popup.js
@@ -1,8 +1,20 @@
 (function () { 
-  document.addEventListener('DOMContentLoaded', function() {
+  //document.addEventListener('DOMContentLoaded', function() {
+  jQuery(function ($) {
     FixIt.util.currentUrl(function(url) {
-      FixIt.Server.list(url, function (r) {
-        document.getElementById('result').innerHTML = url + ':' + r;
+      var domain = URI(url).hostname();
+      $('#new').on('click', function () {
+        FixIt.Server.create({
+          domain : domain
+        }).done(function(person) {
+          console.log('done', person);
+        }).error(function(xhr, ajaxOptions, thrownError) {
+          $('#result').text($('#result').text() + xhr.status);
+          console.log(thrownError);
+        });
+      });
+      FixIt.Server.find(domain, function (r) {
+        $('#result').text(domain + ':' + r);
       });
     });
   });
